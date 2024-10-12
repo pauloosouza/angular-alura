007f1208bdc52c392736d33e33d15aa8d4b09429
Athos Paulo Souza <73041851+pauloosouza@users.noreply.github.com>
2024-10-12 03:02:27 -0300

    Criação do footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, CabecalhoComponent],
+  imports: [RouterOutlet, HomeComponent, CabecalhoComponent, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
