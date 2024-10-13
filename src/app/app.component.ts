f1cba94c38c5062d95c2225f0be2d3e8d619c3e0
Athos Paulo Souza <73041851+pauloosouza@users.noreply.github.com>
2024-10-13 01:48:29 -0300

    Feito a parte visual do formulário de pensamentos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 import { RodapeComponent } from "./componentes/rodape/rodape.component";
+import { CriarPensamentoComponent } from "./componentes/pensamentos/criar-pensamento/criar-pensamento.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, CabecalhoComponent, RodapeComponent],
+  imports: [RouterOutlet, HomeComponent, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
