48afbc87f4ebb18e3e992aebc8163343808f9349
Athos Paulo Souza <73041851+pauloosouza@users.noreply.github.com>
2024-10-14 20:54:31 -0300

    Criado evento dos inputs e do form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,19 @@ import { HomeComponent } from "./home/home.component";
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 import { RodapeComponent } from "./componentes/rodape/rodape.component";
 import { CriarPensamentoComponent } from "./componentes/pensamentos/criar-pensamento/criar-pensamento.component";
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent],
+  imports: [
+    RouterOutlet, 
+    HomeComponent, 
+    CabecalhoComponent, 
+    RodapeComponent, 
+    CriarPensamentoComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
   standalone: true,
-  imports: [],
+  imports: [
+  FormsModule
+  ],
   templateUrl: './criar-pensamento.component.html',
   styleUrl: './criar-pensamento.component.css'
 })
 export class CriarPensamentoComponent {
+  pensamentos = {
+    id:'1',
+    conteudo: 'aprendendo angular',
+    autoria: 'Dev',
+    Modelo: ''
+  }
+  criarPensamentos(){
+    alert("Novo pensamento criado");
+  }
+  cancelarAcao(){
+    alert(" Cancelamento realizado com sucesso !")
+  }
 
 }
